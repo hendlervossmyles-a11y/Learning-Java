e55f1d394baade214f0236e1c0cdc9406b80b1f6
Myles <myles.hendlervoss.25@cnu.edu>
2026-08-25 15:44:50 -0400

    Hello World!

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+// First code
+
+public class HelloWorld { // Everything in Java revolves around classes and methods
+    public static void main(String[] args) { // Void means that the method has no output
+        System.out.println("Hello World"); // print is now println
+    }
+
+}
