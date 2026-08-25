35f0ac7a441ff71f9c0b332b55c0b92fc8af465a
Myles <myles.hendlervoss.25@cnu.edu>
2026-08-25 16:16:53 -0400

    Made static variable work

--- src/StaticPractice.java
+++ src/StaticPractice.java
@@ -2,10 +2,13 @@ public class StaticPractice {
     public static void main(String[] args) {
 
         Cars car1 = new Cars("Hyundai", "Blue");
+        Cars car2 = new Cars("Ford", "Red");
 
         System.out.println(car1.colorOfCar);
         System.out.println(car1.modelOfCar);
-        System.out.println(car1.numberOfCars);
+        System.out.println("Total number of cars:\t" + Cars.totalNumberOfCars);
+
+
 
     }
 }
