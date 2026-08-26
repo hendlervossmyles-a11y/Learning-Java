0b347116b8a9c6706a820b3bc124c32b7c3809ca
Myles <myles.hendlervoss.25@cnu.edu>
2026-08-26 13:44:45 -0400

    I learned that Java really needs instances to work

--- src/LearningMethods.java
+++ src/LearningMethods.java
@@ -4,8 +4,15 @@ public class LearningMethods {
         return sum;
     }
 
-    public void main(String[] args) {
-        int sum = simpleAddition(3,4);
+//    public void main(String[] args) {
+//        int sum = simpleAddition(3,4);  *** To make this work you need to create an instance
+//        System.out.println(sum);             so java has something stored in memory
+//    }
+//}
+
+       public static void main(String[] args) {
+        LearningMethods instance  = new LearningMethods(); // Create an instance that can be used
+        int sum = instance.simpleAddition(3,4);
         System.out.println(sum);
     }
 }
