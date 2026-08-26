38508abd1a66b0b6ca4010eb63c31dd52cd9bb0d
Myles <myles.hendlervoss.25@cnu.edu>
2026-08-26 13:25:02 -0400

    I think this is a working method

---
+++ src/LearningMethods.java
@@ -0,0 +1,10 @@
+public class LearningMethods {
+    public int simpleAddition(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public void main(String[] args) {
+
+    }
+}
