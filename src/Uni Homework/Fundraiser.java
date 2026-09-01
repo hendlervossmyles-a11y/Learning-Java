7e85d4507861af351d48be4b58e66001b2c356e4
Myles Hendler-Voss <myles.hendlervoss.25@cnu.edu>
2026-08-31 20:40:50 -0400

    Finished the fundraiser

---
+++ src/Uni Homework/Fundraiser.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.math.*;
+public class Fundraiser {
+	public static void main(String[] args) {
+		double adult_ticket_cost = 19;
+		double student_ticket_cost = 12.5;
+		System.out.println("Welcome to ticket price calculator!\n\n"+"**Negatives will be converted to positve values**\n\n");
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter # of adult tickets here: ");
+		double  adult_ticket = Math.abs(reader.nextInt());
+		System.out.println("# Entered:" + adult_ticket);
+		System.out.println("Enter # of student tickets here: ");
+		double  student_ticket = Math.abs(reader.nextInt());
+		System.out.println("# Entered: " + student_ticket);
+		
+		double final_cost = (adult_ticket*adult_ticket_cost)+(student_ticket*student_ticket_cost);
+		System.out.println("Final Cost: "+final_cost+"$");
+
+		
+	}
+	
+	
+}
