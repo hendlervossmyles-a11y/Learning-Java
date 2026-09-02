798be38b112c984b30a0f5d2e9f5ab91f07d888b
Myles Hendler-Voss <myles.hendlervoss.25@cnu.edu>
2026-09-02 16:30:09 -0400

    Updated deposits

--- src/Uni Homework/Deposits.java
+++ src/Uni Homework/Deposits.java
@@ -1,4 +1,13 @@
-
+import java.util.Scanner;
 public class Deposits {
-
+	public static void main(String args[]) {
+		int num_days = 0;
+		Scanner reader = new Scanner(System.in);
+		
+		double account_ammount = reader.nextInt();
+		
+		System.out.println(account_ammount);
+	}
+	
+	
 }
