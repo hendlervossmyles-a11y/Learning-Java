32e59cb818d6181e5ca94ec78c4e59c34a70fa3c
Myles <myles.hendlervoss.25@cnu.edu>
2026-09-02 16:40:31 -0400

    Finished the deposits function

--- src/Uni Homework/Deposits.java
+++ src/Uni Homework/Deposits.java
@@ -1,12 +1,20 @@
 import java.util.Scanner;
 public class Deposits {
 	public static void main(String args[]) {
-		int num_days = 0;
+		double num_days = 0;
 		Scanner reader = new Scanner(System.in);
 		
-		double account_ammount = reader.nextInt();
-		
-		System.out.println(account_ammount);
+
+		System.out.println("Input Current Account Value in Dollars: ");
+		double account_amount = reader.nextDouble();
+		System.out.println("Value entered: "+account_amount+"\n");
+		System.out.println("Enter Number of Simulation Days: ");
+		num_days = reader.nextDouble();
+		System.out.println("Number of Days Entered: "+num_days);
+
+		account_amount = account_amount+(num_days*10);
+
+		System.out.println("The value after "+num_days+" days is: "+account_amount);
 	}
 	
 	
